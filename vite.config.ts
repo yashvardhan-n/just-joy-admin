0c9ba73cac77d86eadacf47a66cf4d6d0548fbc7
rudras6272 <rudras1212@gmail.com>
2025-03-09 23:10:05 +0530

    Initial Changes

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server : {
+    proxy : {
+      '/api': 'https://just-joy-backend.vercel.app',
+    }
+  },
   plugins: [react()],
 })
